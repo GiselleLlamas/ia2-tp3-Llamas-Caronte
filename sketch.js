d844e8e27ec90a5661f109f5fee5c3130deca3a8
GiselleLlamas <giselle.llamas@gmail.com>
2025-10-11 18:38:50 -0600

    Initial repository commit!

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  var cnv = createCanvas(1800, 900);
+  // Estoy centrando el sketch, si bien tiene un tamaño arbitrario
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+  background(255, 0, 200);
+}
+
+function draw() {
+  background(220);
+}
