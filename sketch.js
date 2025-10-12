09cefaf9af4fe31a2748c42c363dca841dd01b29
GiselleLlamas <giselle.llamas@gmail.com>
2025-10-11 20:50:18 -0600

    Quick update for debugging

--- sketch.js
+++ sketch.js
@@ -1,12 +1,35 @@
+ // Seteando las variables del sketch 
+
+ //Cambiando los "namespaces" de Matter.js para tener una referencia mas concisa de una sola palabra
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Bodies = Matter.Bodies,
+    Composite = Matter.Composite;
+
+var engine;
+var world;
+var particles = [];
+var constraints = [];
+var caja;
+
 function setup() {
   var cnv = createCanvas(1800, 900);
   // Estoy centrando el sketch, si bien tiene un tamaño arbitrario
   var x = (windowWidth - width) / 2;
   var y = (windowHeight - height) / 2;
   cnv.position(x, y);
-  background(255, 0, 200);
+  engine = Engine.create();
+  world = engine.world;
+  caja = Bodies.rectangle(500, 400, 80, 80);
+  Matter.Runner.run(Runner, [engine]);
+  Composite.add(engine.world, [caja]);
+  //console.log(caja);
 }
 
+
 function draw() {
-  background(220);
+  background(50);
+  Matter.Render.world(render);
+  rect(caja.position.x, caja.position.y, 80,80);
 }
